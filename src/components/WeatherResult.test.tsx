7b0a0f2296df8abb1613b687d955f401dfc24de4
sebcorva <se.corvalan@duocuc.cl>
2026-02-20 08:53:37 -0300

    added new testing

---
+++ src/components/WeatherResult.test.tsx
@@ -0,0 +1,24 @@
+import { describe, test, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import WeatherResult from './WeatherResult';
+
+const mockWeather = {
+    city: 'Santiago',
+    country: 'Chile',
+    temp: 27,
+    last_update_date: '19 de febrero',
+    last_update_time: '10:00',
+    is_day: 0
+}
+
+describe('WeatherResult', () => {
+    test('Should render props of weather propertly', () => {
+        render(<WeatherResult weather={mockWeather}/>);
+
+        expect(screen.getByText('Santiago')).toBeDefined();
+        expect(screen.getByText('Chile')).toBeDefined();
+        expect(screen.getByText(27)).toBeDefined();
+        expect(screen.getByText(/19 de febrero/i)).toBeDefined();
+        expect(screen.getByText(/10:00/i)).toBeDefined();
+    });
+})
